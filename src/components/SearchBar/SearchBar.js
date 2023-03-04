60f5be253d45f8d0cc6cf661dec4a2bef9404fe9
Phong Cao <phongcao0714@gmail.com>
2023-03-04 13:06:29 -0800

    Implement Artist Feature

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,6 +8,7 @@ const NUMBER_OF_OPTIONS = 5
 
 let trackIDs = {}
 let albumIDs = {}
+let artistNames = {}
 
 const SearchBar = (props) => {
   let searchQuery = ''
@@ -19,10 +20,10 @@ const SearchBar = (props) => {
 
     if ((side === 'left' && categoryLeft === 'Tracks') || (side === 'right' && categoryRight === 'Tracks')) {
       Spotify.searchTrack(searchQuery).then(tracksList => {
-        
+
 
         for (let i = 0; i < NUMBER_OF_OPTIONS; i++) {
-          const option = document.getElementById(`option-${i+1}`)
+          const option = document.getElementById(`option-${i + 1}`)
           const track = tracksList[i]
 
           let trackNameAndID = track.name + ` (${track.artist})`
@@ -37,17 +38,32 @@ const SearchBar = (props) => {
       Spotify.searchAlbum(searchQuery).then(albumsList => {
 
         for (let i = 0; i < NUMBER_OF_OPTIONS; i++) {
-          const option = document.getElementById(`option-${i+1}`)
+          const option = document.getElementById(`option-${i + 1}`)
           const album = albumsList[i]
 
           let albumNameAndID = album.name + ` (${album.artist})`
 
           albumIDs[albumNameAndID] = album.id
 
-          option.value = albumNameAndID          
+          option.value = albumNameAndID
         }
       })
-    }
+    } else if ((side === 'left' && categoryLeft === 'Artists') || (side === 'right' && categoryRight === 'Artists')) {
+      Spotify.searchArtist(searchQuery).then(artistsList => {
+        for (let i = 0; i < NUMBER_OF_OPTIONS; i++) {
+          const option = document.getElementById(`option-${i + 1}`)
+
+          
+          const artist = artistsList[i]
+
+          let artistName = artist.name
+
+          artistNames[artistName] = artist.id
+
+          option.value = artistName
+        }
+      })
+    } 
 
   }
 
@@ -59,7 +75,7 @@ const SearchBar = (props) => {
     } else {
       categoryRight = newCategory
     }
-  
+
   }
 
   const checkEnter = (e, side) => {
@@ -77,81 +93,112 @@ const SearchBar = (props) => {
         })
       } else if ((side === 'left' && categoryLeft === 'Albums') || (side === 'right' && categoryRight === 'Albums')) {
         let albumID = albumIDs[searchQuery]
-        
+
         Spotify.getAlbum(albumID).then(data => {
-          
+
           props.sendAlbumData(data, side)
 
-          const getTrackFeaturesSum = async () => {
-            let acousticness = 0;
-            let danceability = 0;
-            let energy = 0;
-            let instrumentalness = 0;
-            let liveness = 0;
-            let loudness = 0;
-            let speechiness = 0;
-            let valence = 0;
-
-            var promiseArray = [];
-
-            for (let i = 0; i < data.tracks.items.length; i++) {
-              promiseArray.push(Spotify.getAudioFeatures(data.tracks.items[i].id).then(attributes => {
-                acousticness += attributes['acousticness']
-                danceability += attributes['danceability']
-                energy += attributes['energy']
-                instrumentalness += attributes['instrumentalness']
-                liveness += attributes['liveness']
-                loudness += attributes['loudness']
-                speechiness += attributes['speechiness']
-                valence += attributes['valence']
-              }))
-            }
-
-            Promise.all(promiseArray).then(() => {
-              acousticness = acousticness / data.tracks.items.length
-              danceability = danceability / data.tracks.items.length
-              energy = energy / data.tracks.items.length
-              instrumentalness = instrumentalness / data.tracks.items.length
-              liveness = liveness / data.tracks.items.length
-              loudness = loudness / data.tracks.items.length
-              speechiness = speechiness / data.tracks.items.length
-              valence = valence / data.tracks.items.length
-
-              let features = {
-                acousticness: acousticness,
-                danceability: danceability,
-                energy: energy,
-                instrumentalness: instrumentalness,
-                liveness: liveness,
-                loudness: loudness,
-                speechiness: speechiness,
-                valence: valence
-              }
-              props.sendAudioFeatures(features, side)
-            })
-
-          }
-          getTrackFeaturesSum()
+          
+          getAndSendTrackFeaturesSum(data, side)
         })
+      } else if ((side === 'left' && categoryLeft === 'Artists') || (side === 'right' && categoryRight === 'Artists')) {
+        let artistID = artistNames[searchQuery]
+
+        Spotify.getArtist(artistID).then(data => {
+
+          props.sendArtistData(data, side)
+        })
+
+        Spotify.getArtistTopTrack(artistID).then(data => {  
+          getAndSendTrackFeaturesSum(data, side)
+        })
+
       }
 
       //Clear search bar suggestions
-      
+
       for (let i = 0; i < NUMBER_OF_OPTIONS; i++) {
-        const option = document.getElementById(`option-${i+1}`)
+        const option = document.getElementById(`option-${i + 1}`)
 
         option.value = ''
       }
 
       e.preventDefault() //Make sure on term change is not called
-      
+
     }
   }
 
+  const getAndSendTrackFeaturesSum = async (data, side) => {
+    let acousticness = 0;
+    let danceability = 0;
+    let energy = 0;
+    let instrumentalness = 0;
+    let liveness = 0;
+    let loudness = 0;
+    let speechiness = 0;
+    let valence = 0;
 
-  return (
-    <div className="search-bar">
+    var promiseArray = [];
 
+    let numOfTracks = 0
+    if ((side === 'left' && categoryLeft === 'Albums') || (side === 'right' && categoryRight === 'Albums')) {
+      for (let i = 0; i < data.tracks.items.length; i++) {
+        numOfTracks = numOfTracks + 1
+        promiseArray.push(Spotify.getAudioFeatures(data.tracks.items[i].id).then(attributes => {
+          acousticness += attributes['acousticness']
+          danceability += attributes['danceability']
+          energy += attributes['energy']
+          instrumentalness += attributes['instrumentalness']
+          liveness += attributes['liveness']
+          loudness += attributes['loudness']
+          speechiness += attributes['speechiness']
+          valence += attributes['valence']
+      }))
+      }
+    } else {
+      for (let i = 0; i < data.tracks.length; i++) {
+        numOfTracks = numOfTracks + 1
+        promiseArray.push(Spotify.getAudioFeatures(data.tracks[i].id).then(attributes => {
+          acousticness += attributes['acousticness']
+          danceability += attributes['danceability']
+          energy += attributes['energy']
+          instrumentalness += attributes['instrumentalness']
+          liveness += attributes['liveness']
+          loudness += attributes['loudness']
+          speechiness += attributes['speechiness']
+          valence += attributes['valence']
+        }))
+      }
+    }
+
+    Promise.all(promiseArray).then(() => {
+      acousticness = acousticness / numOfTracks
+      danceability = danceability / numOfTracks
+      energy = energy / numOfTracks
+      instrumentalness = instrumentalness / numOfTracks
+      liveness = liveness / numOfTracks
+      loudness = loudness / numOfTracks
+      speechiness = speechiness / numOfTracks
+      valence = valence / numOfTracks
+
+      let features = {
+        acousticness: acousticness,
+        danceability: danceability,
+        energy: energy,
+        instrumentalness: instrumentalness,
+        liveness: liveness,
+        loudness: loudness,
+        speechiness: speechiness,
+        valence: valence
+      }
+      
+      props.sendAudioFeatures(features, side)
+    })
+
+  }
+
+  const TrackList = () => {
+    return (
       <datalist id="tracks-list">
         <option id='option-1'></option>
         <option id='option-2'></option>
@@ -159,18 +206,48 @@ const SearchBar = (props) => {
         <option id='option-4'></option>
         <option id='option-5'></option>
       </datalist>
-      <input type="search" list="tracks-list" placeholder="Lookup a Track, Album, or Artist"
-        onChange={(e) => handleTermChange(e, props.direction)}
-        onKeyDown={(e) => checkEnter(e, props.direction)} />
+    )
+  }
+
+  const Select = () => {
+    return (
       <select onChange={() => handleCategoryChange(props.direction)} id={`select-id-${props.direction}`}>
         <option value="Tracks">Tracks</option>
         <option value="Albums">Albums</option>
-        
+        <option value="Artists">Artists</option>
       </select>
+    )
+  }
 
-    </div>
-  )
-  
+  const SearchInput = () => {
+    return (
+      <input type="search" list="tracks-list" placeholder="Lookup a Track, Album, or Artist"
+        onChange={(e) => handleTermChange(e, props.direction)}
+        onKeyDown={(e) => checkEnter(e, props.direction)} />
+    )
+  }
+
+  if (props.direction === "left") {
+    return (
+      <div className="search-bar">
+        <TrackList />
+        <SearchInput/>
+        <Select />
+      </div>
+    )
+  } else {
+    return (
+      <div className="search-bar">
+        <TrackList />
+        <Select />
+        <SearchInput/>
+      </div>
+    )
+  }
+
+
+
+
 
 }
 
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -4,123 +4,129 @@ const REDIRECT_URI = "http://localhost:3000"
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize?"
 const RESPONSE_TYPE = "token"
 const AUTH_URL = AUTH_ENDPOINT +
-    `client_id=${CLIENT_ID}&` +
-    `response_type=${RESPONSE_TYPE}&` +
-    `redirect_uri=${REDIRECT_URI}`;
+  `client_id=${CLIENT_ID}&` +
+  `response_type=${RESPONSE_TYPE}&` +
+  `redirect_uri=${REDIRECT_URI}`;
 
 
 const Spotify = {
-    getAccessToken() {
-        if (accessToken) return accessToken;
-        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-        if (accessTokenMatch && expiresInMatch) {
-            accessToken = accessTokenMatch[1];
-            const expiresToken = Number(expiresInMatch[1]);
-            window.setTimeout(() => accessToken = '', expiresToken * 1000);
-            window.history.pushState('Access Token', null, '/');
-            return accessToken;
-        } else {
-            window.location = AUTH_URL;
-        }
-    }, searchTrack(searchTerm) {
-        accessToken = Spotify.getAccessToken()
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
-          headers: { Authorization: `Bearer ${accessToken}` }
-        }).then(response => {
-          
-          return response.json();
-        }).then(jsonResponse => {
-          
-          if (!jsonResponse.tracks) return [];
-          return jsonResponse.tracks.items.map(track => ({
-            id: track.id,
-            name: track.name,
-            artist: track.artists[0].name,
-            album: track.album.name,
-            uri: track.uri,
-            popularity: track.popularity,
-            image: track.album.images[0]
-          }))
-        }).then(searchResults => {
-          return searchResults
-        });
-      }, getAudioFeatures(trackID) {
-        accessToken = Spotify.getAccessToken()
-        return fetch(`https://api.spotify.com/v1/audio-features/${trackID}`, {
-          headers: { Authorization: `Bearer ${accessToken}` }
-        }).then(response => {
-          
-          return response.json();
-        }).then(jsonResponse => {
-          return jsonResponse
-        });
-      }, getTrack(trackID) {
-        accessToken = Spotify.getAccessToken()
-        return fetch(`https://api.spotify.com/v1/tracks/${trackID}`, {
-          headers: { Authorization: `Bearer ${accessToken}` }
-        }).then(response => {
-          
-          return response.json();
-        }).then(jsonResponse => {
-          return jsonResponse
-        });
-      }, searchAlbum(searchTerm) {
-        accessToken = Spotify.getAccessToken()
-        return fetch(`https://api.spotify.com/v1/search?type=album&q=${searchTerm}`, {
-          headers: { Authorization: `Bearer ${accessToken}` }
-        }).then(response => {
-          
-          return response.json();
-        }).then(jsonResponse => {
-          if (!jsonResponse.albums) return [];
-          return jsonResponse.albums.items.map(album => ({
-              name: album.name,
-              artist: album.artists[0].name,
-              id: album.id
-          }))
-        }).then(searchResults => {
-          return searchResults
-        });
-      }, getAlbum(albumID) {
-        accessToken = Spotify.getAccessToken()
-        return fetch(`https://api.spotify.com/v1/albums/${albumID}`, {
-          headers: { Authorization: `Bearer ${accessToken}` }
-        }).then(response => {
-          
-          return response.json();
-        }).then(jsonResponse => {
-          return jsonResponse
-        });
-      }, searchArtist(searchTerm) {
-        accessToken = Spotifyt.getAccessToken()
-        return fetch(`https://api.spotify.com/v1/search?type=artist&q=${searchTerm}`, {
-          headers: {Authorization: `Bearer ${accessToken}`}
-        }).then(response => {
+  getAccessToken() {
+    if (accessToken) return accessToken;
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresToken = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresToken * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      window.location = AUTH_URL;
+    }
+  }, searchTrack(searchTerm) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
 
-          return response.json();
-        }).then(jsonResponse => {
-          if (!jsonResponse.artist) return [];
-          return jsonResponse.artist.items.map(artist => ({
-            name: artist.name,
-            id: artist.id
-            // toptracks?
-          }))
-        }).then(searchResults => {
-          return searchResults
-        });
-      }, getArtist(artistID) {
-        accessToken = Spotify.getAccessToken()
-        return fetch(`https://api.spotify.com/v1/albums/${artistID}`, {
-          headers: {Authorization: `Bearer ${accessToken}` }
-        }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
 
-          return response.json();
-        }).then(jsonResponse => {
-          return jsonResponse
-        });
-      }
-    
+      if (!jsonResponse.tracks) return [];
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri,
+        popularity: track.popularity,
+        image: track.album.images[0]
+      }))
+    }).then(searchResults => {
+      return searchResults
+    });
+  }, getAudioFeatures(trackID) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/audio-features/${trackID}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
+
+      return response.json();
+    }).then(jsonResponse => {
+      return jsonResponse
+    });
+  }, getTrack(trackID) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/tracks/${trackID}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
+
+      return response.json();
+    }).then(jsonResponse => {
+      return jsonResponse
+    });
+  }, searchAlbum(searchTerm) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/search?type=album&q=${searchTerm}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
+
+      return response.json();
+    }).then(jsonResponse => {
+      if (!jsonResponse.albums) return [];
+      return jsonResponse.albums.items.map(album => ({
+        name: album.name,
+        artist: album.artists[0].name,
+        id: album.id
+      }))
+    }).then(searchResults => {
+      return searchResults
+    });
+  }, getAlbum(albumID) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/albums/${albumID}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
+
+      return response.json();
+    }).then(jsonResponse => {
+      return jsonResponse
+    });
+  }, searchArtist(searchTerm) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/search?type=artist&q=${searchTerm}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (!jsonResponse.artists) return [];
+      return jsonResponse.artists.items.map(artist => ({
+        name: artist.name,
+        id: artist.id
+      }))
+    }).then(searchResults => {
+      return searchResults
+    });
+  }, getArtist(artistID) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/artists/${artistID}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      return jsonResponse
+    });
+  }, getArtistTopTrack(artistID) {
+    accessToken = Spotify.getAccessToken()
+    return fetch(`https://api.spotify.com/v1/artists/${artistID}/top-tracks?country=US`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      return jsonResponse
+    });
+  }
+
 };
 
 
