19332e74aa52ea19b43b8c5c15080e8d706cdc81
Phong Cao <phongcao0714@gmail.com>
2023-03-27 12:20:25 -0700

    Configure React app for deployment on Github

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 import { useEffect, useState } from 'react';
 
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, Navigate ,useNavigate } from 'react-router-dom';
 
 import Spotify from './util/Spotify';
 
 import { LandingPage } from './components/LandingPage/LandingPage';
 import { ComparisonPage } from './components/ComparisonPage/ComparisonPage';
 
+let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
 
 function App() {
   const [token, setToken] = useState("")
@@ -44,8 +45,9 @@ function App() {
       <header>
         <a onClick={navigateLandingPage}><h2>SPOTIFY <span className='spotify-text'>COMPARE</span></h2></a>
       </header>
+
       <Routes>
-        <Route path='/' element={<LandingPage onClick={navigateComparisonPage}/>} />
+        <Route path='/LandingPage' element={<LandingPage onClick={navigateComparisonPage}/>} />
         <Route path='/ComparisonPage' element={<ComparisonPage/>} />
       </Routes>
 
